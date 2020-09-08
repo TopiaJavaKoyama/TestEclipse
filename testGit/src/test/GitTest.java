8ca72e059eed1636f88fe597ff1a9a7d4a4d5c5c
Tatsuya Koyama <koyama@topiapc.com>
2020-09-09 02:03:48 +0900

    Heroミナト追加

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
+		Hero minato = new Hero("ミナト",100);
+		System.out.println(minato.getName());
 	}
 
 }
